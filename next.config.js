716eb331d5351ac1a9cb52e9062531c063fb1bfb
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-04-16 13:47:44 -0500

    Log sw path

--- next.config.js
+++ next.config.js
@@ -5,3 +5,7 @@ module.exports = withOffline({
     ? process.env.GITHUB_REPOSITORY.split('/')[1]
     : '/'
 })
+
+console.log(process.env.GITHUB_REPOSITORY !== undefined
+    ? process.env.GITHUB_REPOSITORY.split('/')[1]
+    : '/', process.env)
