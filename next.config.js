389e3b262bf87ce41e75c585d75a4d708ad32ebd
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-04-16 13:50:45 -0500

    /repo

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ const withOffline = require('next-offline')
 
 module.exports = withOffline({
   path: process.env.GITHUB_REPOSITORY !== undefined
-    ? process.env.GITHUB_REPOSITORY.split('/')[1]
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
     : '/'
 })
-
-console.log(process.env.GITHUB_REPOSITORY !== undefined
-    ? process.env.GITHUB_REPOSITORY.split('/')[1]
-    : '/', process.env)
