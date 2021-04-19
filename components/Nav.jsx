f63e472f7a86e8d6bf2b0d65d598fedfede7fb78
KhajasJS <>
2021-04-19 17:49:20 +0000

    Nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,12 +1,34 @@
 import { useRouter } from 'next/router'
+import { Breadcrumb, Menu } from 'antd'
+import Link from 'next/link'
 
-const headers = {
-  '/': 'Lists',
-  '/pictures': 'Pictures'
-}
+const headers = new Map()
+  .set('/', 'Lists')
+  .set('/pictures', 'Pictures')
+
+const menu = (
+  <Menu>
+    {[...headers].map(([page, header]) => (
+      <Menu.Item>
+      <Link href={page}>
+        {header}
+      </Link>
+    </Menu.Item>
+    ))}
+  </Menu>
+)
 
 const Nav = () => {
   const { route } = useRouter()
 
-  
+  <Breadcrumb>
+    <Breadcrumb.Item>
+      <img src='/icon-192px.png' width={50} height={50} />
+    </Breadcrumb.Item>
+    <Breadcrumb.Item overlay={menu}>
+      <a href=''>{headers.get(route)}</a>
+    </Breadcrumb.Item>
+  </Breadcrumb>
 }
+
+export default Nav
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,5 @@
 import Head from 'next/head'
-import { Breadcrumb, Menu } from 'antd'
-import Link from 'next/link'
-
-const menu = (
-  <Menu>
-    <Menu.Item>
-      <Link href='/'>
-        Lists
-      </Link>
-    </Menu.Item>
-    <Menu.Item>
-      <Link href='/pictures'>
-        Pictures
-      </Link>
-    </Menu.Item>
-  </Menu>
-)
+import Nav from '../components/Nav'
 
 const App = () => {
   return (
@@ -24,14 +8,7 @@ const App = () => {
         <title>Picture List</title>
       </Head>
       <h1>Picture List</h1>
-      <Breadcrumb>
-        <Breadcrumb.Item>
-          <img src='/icon-192px.png' width={50} height={50} />
-        </Breadcrumb.Item>
-        <Breadcrumb.Item overlay={menu}>
-          <a href=''>Lists</a>
-        </Breadcrumb.Item>
-      </Breadcrumb>
+      <Nav />
     </>
   )
 }
