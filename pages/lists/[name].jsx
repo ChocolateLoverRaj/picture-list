72fa2a611b7316d5420756d83dd4ddb64185a514
KhajasJS <>
2021-04-22 17:30:49 +0000

    Add item modal

--- pages/lists/[name].jsx
+++ pages/lists/[name].jsx
@@ -1,14 +1,66 @@
 import { mainTitle, listsTitle } from '../../lib/titles'
 import Title from '../../components/Title'
 import { useRouter } from 'next/router'
+import GlobalContext from '../contexts/GlobalContext'
+import {
+  Statistic,
+  Button,
+  Modal
+} from 'antd'
+import {
+  PlusOutlined
+} from '@ant-design/icons'
+import {
+  useContext,
+  useState
+} from 'react'
 
 const ListPage = () => {
   const { query: { name } } = useRouter()
 
+  const [
+    lists, 
+    setLists
+  ] = useContext(GlobalContext)
+
+  const list = lists.find({ 
+    name: currentName 
+  }) => currentName === name
+
+  const [
+    adding, 
+    setAdding
+  ] = useState(false)
+
+  const handleAdd = () => {
+    setAdding(true)
+  }
+
+  const handleCancel = () => {
+    setAdding(false)
+  }
+
   return (
     <>
       <Title paths={[name, listsTitle, mainTitle]} />
-      <h1>List Page Coming Soon</h1>
+      <Statistic
+        title='Items'
+        value={list.length}
+      />
+      <Button
+        type='primary'
+        icon={<PlusOutlined />}
+        onClick={handleAdd}
+      >
+        Add Item
+      </Button>
+      <Modal
+        visible={adding}
+        title='Add Item'
+        onCancel={handleCancel}
+      >
+        Add form coming soon
+      </Modal>
     </>
   )
 }
