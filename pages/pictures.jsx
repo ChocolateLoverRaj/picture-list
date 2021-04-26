85805e8bdad5fcfd01cf994582205687202796b3
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-04-26 13:20:14 -0500

    Pictures page shows pictures

--- pages/pictures.jsx
+++ pages/pictures.jsx
@@ -1,12 +1,40 @@
-import { picturesTitle, mainTitle } from '../lib/titles'
-import Title from '../components/Title'
+import { picturesTitle, mainTitle } from "../lib/titles";
+import Title from "../components/Title";
+import { Table, Image, Typography } from "antd";
+import GlobalContext from "../contexts/Global";
+import { useContext } from "react";
+
+const { Paragraph } = Typography;
 
 const PicturesPage = () => {
+  const [{ pictures }] = useContext(GlobalContext).pictures;
+
   return (
     <>
       <Title paths={[picturesTitle, mainTitle]} />
+      <Table
+        columns={[
+          {
+            title: "Name",
+            render: (picture) => <>{picture.name}</>
+          },
+          {
+            title: "Description",
+            render: (picture) => (
+              <Paragraph ellipsis={{ expandable: true, rows: 2 }}>
+                {picture.description}
+              </Paragraph>
+            )
+          },
+          {
+            title: "Picture",
+            render: (picture) => <Image height={50} src={picture.picture} />
+          }
+        ]}
+        dataSource={pictures}
+      />
     </>
-  )
-}
+  );
+};
 
-export default PicturesPage
+export default PicturesPage;
