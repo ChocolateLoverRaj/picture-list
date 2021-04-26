1a2998df6547818dbf7aee702d2b6786d65a2a07
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2021-04-26 13:28:00 -0500

    List Used By

--- pages/pictures.jsx
+++ pages/pictures.jsx
@@ -1,13 +1,17 @@
 import { picturesTitle, mainTitle } from "../lib/titles";
 import Title from "../components/Title";
-import { Table, Image, Typography } from "antd";
+import { Table, Image, Typography, List } from "antd";
 import GlobalContext from "../contexts/Global";
 import { useContext } from "react";
+import Link from "next/link";
 
 const { Paragraph } = Typography;
 
 const PicturesPage = () => {
-  const [{ pictures }] = useContext(GlobalContext).pictures;
+  const {
+    lists: [lists],
+    pictures: [{ pictures }]
+  } = useContext(GlobalContext);
 
   return (
     <>
@@ -29,6 +33,22 @@ const PicturesPage = () => {
           {
             title: "Picture",
             render: (picture) => <Image height={50} src={picture.picture} />
+          },
+          {
+            title: "Used By",
+            render: (picture) => (
+              <List
+                dataSource={lists.filter(
+                  ({ items }) =>
+                    items.find(({ id }) => id === picture.id) !== undefined
+                )}
+                renderItem={(list) => (
+                  <List.Item>
+                    <Link href="">{list.name}</Link>
+                  </List.Item>
+                )}
+              />
+            )
           }
         ]}
         dataSource={pictures}
